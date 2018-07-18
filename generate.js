52ce370048f87eed6e35eba3ed5cab77229a3025
Marcellino Ornelas <marcellino.ornelas.com>
2018-07-18 13:57:22 -0700

    starting generate data functionality

--- generate.js
+++ generate.js
@@ -1,1 +1,13 @@
-var db = require('./models/');
+const mongoose = require('mongoose');
+const faker = require('faker');
+
+const db = require('./models/');
+
+// drop cellection if exists
+// mongoose.connection.collections['photos'].drop( function(err) {
+//   console.log('collection dropped');
+// });
+
+for ( let i = 1; i <= 100; i++ ) {
+  
+}
