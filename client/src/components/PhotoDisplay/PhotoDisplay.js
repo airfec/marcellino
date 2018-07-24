2626ee710c3ce016464d20d2c39b21859d9c9a97
Marcellino Ornelas <marcellino.ornelas.com>
2018-07-24 13:00:11 -0700

    added icons to main display buttons

--- client/src/components/PhotoDisplay/PhotoDisplay.js
+++ client/src/components/PhotoDisplay/PhotoDisplay.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Fa from 'react-fontawesome';
 
 const PhotoDisplay = props => {
   if (!props.photo) {
@@ -9,8 +10,12 @@ const PhotoDisplay = props => {
       <img src={props.photo.photo_url} alt="main photo" />
       <div className="gallery-display fx">
         <div className="gallery-display-top fx">
-          <button className="btn">Share</button>
-          <button className="btn">Save</button>
+          <button className="btn">
+            <Fa name="share" /> Share
+          </button>
+          <button className="btn">
+            <Fa name="heart-o" /> Save
+          </button>
         </div>
         <div className="gallery-display-bottom">
           <button className="btn" onClick={props.showCarousel}>
