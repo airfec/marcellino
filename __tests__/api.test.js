3727e3ac23cd1b51a3057e308a256a52c38df014
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-08-07 12:48:35 -0700

    Implement CRUD test structure and clean up code

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,18 +1,17 @@
-const app = require('../server/app');
+const app = require("../server/app");
+const request = require("supertest");
 
-const request = require('supertest');
-
-describe('API', () => {
-  describe('GET /api/rooms/:id/photos', () => {
-    test('should respond with status code 200', () => {
+describe("API", () => {
+  describe("GET /api/rooms/:id/photos", () => {
+    test("should respond with status code 200", () => {
       return request(app)
-        .get('/api/rooms/1/photos')
+        .get("/api/rooms/1/photos")
         .expect(200);
     });
 
-    test('should respond with a array of Photo objects', () => {
+    test("should respond with a array of Photo objects", () => {
       return request(app)
-        .get('/api/rooms/1/photos')
+        .get("/api/rooms/1/photos")
         .expect(200)
         .expect(res => {
           expect(res.body.results).toEqual(
@@ -28,29 +27,97 @@ describe('API', () => {
         });
     });
 
-    test('should respond with correct values', () => {
+    test("should respond with correct values", () => {
       return request(app)
-        .get('/api/rooms/1/photos')
+        .get("/api/rooms/1/photos")
         .expect(200)
         .expect(res => {
           const item = res.body.results[0];
-
           expect(item.room_id).toEqual(expect.any(Number));
           expect(item.room_id).toBeGreaterThanOrEqual(0);
           expect(item.room_id).toBeLessThanOrEqual(100);
-
           expect(item.description).toEqual(expect.any(String));
-
           expect(item.verified).toEqual(expect.any(Boolean));
-
           expect(item.photo_url).toEqual(expect.any(String));
         });
     });
 
-    test('should respond with a 404 for invalid id', () => {
+    test("should respond with a 404 for invalid id", () => {
       return request(app)
-        .get('/api/rooms/101/photos')
+        .get("/api/rooms/101/photos")
         .expect(404);
     });
-  }); // desc for GET /api/photos
-}); // main desc
+  });
+
+  describe("POST /api/rooms/:id/photos", () => {
+    test("should respond with status code 200", () => {
+      return request(app)
+        .post("/api/rooms/1/photos")
+        .expect(200);
+    });
+
+    test("should respond with a 404 for invalid id", () => {
+      return request(app)
+        .post("/api/rooms/101/photos")
+        .expect(404);
+    });
+
+    test("should attempt to add photo to db", () => {
+      expect(true).toEqual(false);
+    });
+  });
+
+  describe("POST /api/rooms/:id/photos", () => {
+    test("should respond with status code 200", () => {
+      return request(app)
+        .post("/api/rooms/1/photos")
+        .expect(200);
+    });
+
+    test("should respond with a 404 for invalid id", () => {
+      return request(app)
+        .post("/api/rooms/101/photos")
+        .expect(404);
+    });
+
+    test("should attempt to add photo to db", () => {
+      expect(true).toEqual(false);
+    });
+  });
+
+  describe("PUT /api/rooms/:id/photos", () => {
+    test("should respond with status code 200", () => {
+      return request(app)
+        .put("/api/rooms/1/photos")
+        .expect(200);
+    });
+
+    test("should respond with a 404 for invalid id", () => {
+      return request(app)
+        .put("/api/rooms/101/photos")
+        .expect(404);
+    });
+
+    test("should attempt to edit photo in db", () => {
+      expect(true).toEqual(false);
+    });
+  });
+
+  describe("DELETE /api/rooms/:id/photos", () => {
+    test("should respond with status code 200", () => {
+      return request(app)
+        .delete("/api/rooms/1/photos")
+        .expect(200);
+    });
+
+    test("should respond with a 404 for invalid id", () => {
+      return request(app)
+        .delete("/api/rooms/101/photos")
+        .expect(404);
+    });
+
+    test("should attempt to delete photo in db", () => {
+      expect(true).toEqual(false);
+    });
+  });
+});
--- generate.js
+++ generate.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
-const faker = require('faker');
-const path = require('path');
+const mongoose = require("mongoose");
+const faker = require("faker");
+const path = require("path");
 
-const db = require('./models/');
+const db = require("./models/");
 
-const IMG_URL = 'https://s3-us-west-1.amazonaws.com/airfec2018/photos/file-';
+const IMG_URL = "https://s3-us-west-1.amazonaws.com/airfec2018/photos/file-";
 
 const MAX_ID_RANGE = 100;
 const MAX_IMG_RANGE = 10;
@@ -15,40 +15,40 @@ function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
 }
 
-// drop cellection if exists
-console.log('cleared db for re-seed...\n');
+// drop collection if exists
+console.log("cleared db for re-seed...\n");
 db.Photo.remove({}).exec(function(err, results) {
   if (err) {
     console.error(err);
     return process.exit(0);
   }
 
-  console.log('db cleared!\n');
-  console.log('starting re-seeding....\n');
+  console.log("db cleared!\n");
+  console.log("starting re-seeding....\n");
 
-  var inProgressDataBaseEntrys = [];
+  var inProgressDataBaseEntries = [];
 
   for (let id = 1; id <= MAX_ID_RANGE; id++) {
-    for (let imgs = 1; imgs <= MAX_IMG_RANGE; imgs++) {
+    for (let images = 1; images <= MAX_IMG_RANGE; images++) {
       var photo = new db.Photo({
         room_id: id,
-        photo_url: IMG_URL + getRandomIntInclusive(1, 75) + '.jpg',
+        photo_url: IMG_URL + getRandomIntInclusive(1, 75) + ".jpg",
         verified: !Math.floor(Math.random() * 2),
         description: faker.lorem.sentence()
       });
 
-      inProgressDataBaseEntrys.push(
+      inProgressDataBaseEntries.push(
         photo.save().then(item => {
-          console.log('photo #' + item.id + ' was created');
+          console.log("photo #" + item.id + " was created");
           return Promise.resolve(item);
         })
       );
     }
   }
 
-  Promise.all(inProgressDataBaseEntrys)
+  Promise.all(inProgressDataBaseEntries)
     .then(function(results) {
-      console.log(results.length + ' entrys saved in DataBase');
+      console.log(results.length + " entries saved in DataBase");
     })
     .catch(function(err) {
       console.error(err);
