afd9334382b7ec7fe776f49c21709eb7c3b28d27
Nicholas Vincent-Hill <nvincenthill@gmail.com>
2018-08-09 16:57:07 -0700

    Implement data generation with createWriteStream

--- generate.js
+++ generate.js
@@ -1,61 +1,35 @@
-const mongoose = require('mongoose');
-const faker = require('faker');
-const path = require('path');
+// generate primary key table
 
-const db = require('./models/');
+const faker = require('faker');
+const fs = require('fs');
 
 const IMG_URL = 'https://s3-us-west-1.amazonaws.com/airfec2018/photos/file-';
-
-const MAX_ID_RANGE = 100;
+const start = 1;
+const MAX_ID_RANGE = start + 1000;
 const MAX_IMG_RANGE = 10;
 
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
-}
+const getRandomIntInclusive = (min, max) => {
+  const minRounded = Math.ceil(min);
+  return Math.floor(Math.random() * (Math.floor(max) - minRounded + 1)) + minRounded;
+};
 
-// drop cellection if exists
-console.log('cleared db for re-seed...\n');
-db.Photo.remove({}).exec(function(err, results) {
-  if (err) {
-    console.error(err);
-    return process.exit(0);
-  }
+console.time('data');
 
-  console.log('db cleared!\n');
-  console.log('starting re-seeding....\n');
-
-  var inProgressDataBaseEntrys = [];
-
-  for (let id = 1; id <= MAX_ID_RANGE; id++) {
-    for (let imgs = 1; imgs <= MAX_IMG_RANGE; imgs++) {
-      var photo = new db.Photo({
-        room_id: id,
-        photo_url: IMG_URL + getRandomIntInclusive(1, 75) + '.jpg',
-        verified: !Math.floor(Math.random() * 2),
-        description: faker.lorem.sentence()
-      });
-
-      inProgressDataBaseEntrys.push(
-        photo.save().then(item => {
-          console.log('photo #' + item.id + ' was created');
-          return Promise.resolve(item);
-        })
-      );
+const generateData = () => {
+  let csv = '';
+  const stream = fs.createWriteStream('data/data.csv');
+  for (let id = start; id <= MAX_ID_RANGE; id += 1) {
+    for (let images = 1; images <= MAX_IMG_RANGE; images += 1) {
+      const name = `room${id}`;
+      const url = `${IMG_URL + getRandomIntInclusive(1, 75)}.jpg`;
+      const verified = !Math.floor(Math.random() * 2);
+      const desc = faker.lorem.sentence();
+      csv += `${id},${name},${url},${verified},${desc},\n`;
+      stream.write(csv);
     }
   }
+  stream.end();
+};
 
-  Promise.all(inProgressDataBaseEntrys)
-    .then(function(results) {
-      console.log(results.length + ' entrys saved in DataBase');
-    })
-    .catch(function(err) {
-      console.error(err);
-    })
-    .then(function() {
-      mongoose.connection.close(function() {
-        process.exit(0);
-      });
-    });
-});
+setTimeout(() => generateData(), 0);
+console.timeEnd('data');
